d7f5e2e7661b518c8a69ddf35e244e23ccfca526
Ztwen-Oströgrasdki <houndekz@gmail.com>
2025-07-19 06:17:37 +0100

    correction test: UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'pseudo' => fake()->name(),
+            'identifiant' => Str::random(16),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
